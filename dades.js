a0170a392e9a555e389d07bf38f0b0a39b87ca09
= <=>
2019-10-03 10:37:26 +0200

    dades refetes

--- dades.js
+++ dades.js
@@ -1,24 +1,18 @@
-db.carreto.insertMany([
-    {
-        _id: "C0001", // Construir amb objectID
-        id_usuari: "U0001",
-        data: new Date(), // L'última actualització
-        productes: [
-            { // NOTA: Per al "carreto" no guardem totes les dades d'un objecte de la col·lecció producte. Només aquelles necessàries per a les utilitats del "carreto"
-                _id: "P0001",
-                nom: "ProducteProva1",
-                preu: 75.00,
-                unitats: 3,
-                descompte: 0.10
-            }
-        ],
-        estat: "pendent", // Si la clau estat no hi figura, es considera pendent. Estats possibles: ["pendent", "completat", ("entregat")]
-        preuTotal: 1
-    }
-    // Afegir aquí més carretons.
+db.categories.insertMany([
+	{"nom":"Teclats"},
+	{"nom":"Pianos", "superior":"Teclats"},
+	{"nom":"Acordions", "superior":"Teclats"},
+	{"nom":"Corda"},
+	{"nom":"Guitarres", "superior":"Corda"},
+	{"nom":"Arc", "superior":"Corda"},
+	{"nom":"Arpes", "superior":"Corda"},
+	{"nom":"Vent"},
+	{"nom":"Vent Fusta", "superior":"Vent"},
+	{"nom":"Vent Metall", "superior":"Vent"},
+	{"nom":"Percussió"}
 ])
 
-db.producte.insertMany([
+db.productes.insertMany([
    // Insertar productes aquí 
 ]);
 
@@ -26,6 +20,43 @@ db.usuari.insertMany([
     // insertar usuaris aquí
 ])
 
-db.categoria.insertMany([
-    // Insertar categories aquí
+
+/*
+
+db.categories = {
+	_id : ObjectId,
+	nom : nomCategoria,
+	superior : nomCategoriaSuperior
+}
+
+db.productes = {
+	_id : codi,
+	nom : nomProducte,
+	categoria_id : ObjectId,
+	detalls : detallsProducte, // string descripció
+	fabricant : fabricantProducte,
+	preu : preuProducte,
+	stock : stockProducte,
+	dades : Object
+}
+
+
+db.carreto.insertMany([
+    {
+        _id: (ObjectID()),
+        id_usuari: "",
+        data: new Date(), // L'última actualització
+        productes: [
+            { // NOTA: Per al "carreto" no guardem totes les dades d'un objecte de la col·lecció producte. Només aquelles necessàries per a les utilitats del "carreto"
+                _id: codi, // Id del producte
+                nom: "ProducteProva1",
+                preu: 75.00,
+                unitats: 3
+            }
+        ],
+        estat: "pendent", // Si la clau estat no hi figura, es considera pendent. Estats possibles: ["pendent", "completat"]
+        preuTotal: 1
+    }
+    // Afegir aquí més carretons.
 ])
+*/
